d83f5df7409bddfcd68a5cc02bca37be2cae2ba6
Philip <philipedwards81@live.co.uk>
2021-03-10 16:47:25 +0000

    navcss
    logo

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div>
       <ul className="navcss">
         <input className="inputBox"type="text" placeholder="Search..."></input>
-        
+       <img id="logo" src="../images/gamelogo.png" alt="logotop"/>
         <div className="linkList">
         <li><a href="/genre">Genre</a></li>
         <li><a href="/topgames">Top Games</a></li>
