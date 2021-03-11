a1063aae7be7e08c9bf5bd873ff07b9580aaf78e
Joe Foster <jfoster29492@gmail.com>
2021-03-11 11:36:52 +0000

    various updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,10 @@ import "./index.css";
 import React, {Component} from "react";
 import './App.css';
 import EditorPicks from "./components/editor-picks/EditorPicks"
-import TopGames from './components/TopGames/TopGames';
+import TopGames from './components/top-games/TopGames';
 import Navbar from './components/navbar/navbar.js';
-import Advert from './components/Adverts/Adverts.js';
+import Advert from './components/adverts/Adverts.js';
 import Platforms from './components/platforms/Platforms.js';
-
 import Footer from './components/footer/Footer'
 import 'font-awesome/css/font-awesome.min.css';
 
