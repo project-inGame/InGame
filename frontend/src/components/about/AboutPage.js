a66cdb2b9b0cbfc97df5444e27165ce66acad513
Joe Foster <jfoster29492@gmail.com>
2021-03-12 16:06:52 +0000

    hotfix

--- frontend/src/components/about/AboutPage.js
+++ frontend/src/components/about/AboutPage.js
@@ -7,8 +7,10 @@ const AboutPage = () => {
   <div className="aboutPageArea">
     <h1 id="aboutTitle">About INGAME</h1>
     <img id="logo" src="../images/gamelogo.png" a href="https://github.com/project-inGame/InGame" alt="logotop" />
-    <p>
-      Created by <a href="https://github.com/Riley142">Riley</a><a href="https://github.com/mafromist">Muge</a><a href="https://github.com/JoeFoster-cn">Joe</a>and<a href="https://github.com/phiddle">Phil</a>
+    <p id="blurb1">------------------</p>
+    <p id="blurb2">
+      <div>Created by</div>
+      <span><a href="https://github.com/Riley142">Riley</a><a href="https://github.com/mafromist">Muge</a><a href="https://github.com/JoeFoster-cn">Joe</a><a href="https://github.com/phiddle">Phil</a></span>
       {" "}
     </p>
   </div>
