de6ea58b0205d7cee0ddce9fd785ae732da5c7e6
Riley <rileyxcroft@gmail.com>
2021-03-12 16:10:45 +0000

    Fixed Editor picks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -54,15 +54,8 @@ function App() {
 function Home() {
   return (
     <div className="App">
-      
-      {/* <Game id="604aabd57be61a6696af8b23" />
-      <Game id="604aaa937be61a6696af8b1f" /> 
-      <Game id="604aa9987be61a6696af8b1a" /> 
-      <Game id="604aaa6b7be61a6696af8b1e" /> */}
-
+    
       <EditorPicks />
-
-     
 
       <div className="mainArea">
         <div className="mainBar">
--- frontend/src/components/Game/Game.js
+++ frontend/src/components/Game/Game.js
@@ -2,14 +2,11 @@ import React, { useState, useEffect } from "react";
 import "./Game.css";
 
 const Game = (props) => {
-
   //fetch single game - just change the id - use insomnia or compass or game._id
   const [game, setGame] = useState([]);
   useEffect(() => {
     const getter = async () => {
-      const response = await fetch(
-        `http://localhost:5000/games/${props.id}`
-      );
+      const response = await fetch(`http://localhost:5000/games/${props.id}`);
       const data = await response.json();
       setGame(data);
     };
@@ -20,60 +17,50 @@ const Game = (props) => {
     <>
       <div className="gameWrapper2">
         <div className="testingGames">
-       
-            <div className="gameContainer2 containerStyle" >
-              <div className="gameBasicInfo">
-                <div className="titleAndPlatform2">
-                    <h1>{game.title}</h1>
-                    <div className="platformAndScore">
-                      <h2>· {game.platform} ·</h2>
-                      <h3>
-                        <span className="thiccc"> Score: </span>{" "}
-                        <span className="gameScore2"> {game.score}</span>{" "}
-                      </h3>
-                  </div>
-                  
+          <div className="gameContainer2 containerStyle">
+            <div className="gameBasicInfo">
+              <div className="titleAndPlatform2">
+                <h1>{game.title}</h1>
+                <div className="platformAndScore">
+                  <h2>· {game.platform} ·</h2>
+                  <h3>
+                    <span className="thiccc"> Score: </span>{" "}
+                    <span className="gameScore2"> {game.score}</span>{" "}
+                  </h3>
                 </div>
-                <img src={game.poster} width="150px" alt="poster"></img>
+              </div>
+              <div className="gameData2">
+                <img
+                  src={game.poster}
+                  width="170px"
+                  height="210px"
+                  alt="poster"
+                ></img>
 
-                {/* Extra data if we want it later? */}
-                {/* <div className="gameData2">
-                  <img src={game.poster} width="200px" alt="poster"></img>
-
-                  <div className="gameDetails">
-                    <p>
-                      <span className="thiccc lightGreen "> Genre: </span>{" "}
-                      {game.genre}{" "}
-                    </p>
-                    <p>
-                      <span className="thiccc lightPink"> Made by: </span>{" "}
-                      {game.gameDeveloper}{" "}
-                    </p>
-                    <p>
-                      <span className="thiccc lightPurple">
-                        {" "}
-                        Release date:{" "}
-                      </span>{" "}
-                      {game.releaseDate}{" "}
-                    </p>
-                    <p>
-                      <span className="thiccc lightOrange">
-                        {" "}
-                        See on different platforms:{" "}
-                      </span>{" "}
-                      INSERT A LINK TO PLATFORMS{" "}
-                    </p>
-                  </div>
-                </div> */}
-
-                <div className="gameSummary">
-                  <h2>Summary:</h2>
-                  <p>{game.summary}</p>
+                <div className="gameDetails">
+                  <p>
+                    <span className="thiccc lightGreen "> Genre: </span>{" "}
+                    {game.genre}{" "}
+                  </p>
+                  <p>
+                    <span className="thiccc lightPink"> Made by: </span>{" "}
+                    {game.gameDeveloper}{" "}
+                  </p>
+                  <p>
+                    <span className="thiccc lightPurple"> Release date: </span>{" "}
+                    {game.releaseDate}{" "}
+                  </p>
+                  <p>
+                    <span className="thiccc lightOrange">
+                      {" "}
+                      See on different platforms:{" "}
+                    </span>{" "}
+                    INSERT A LINK TO PLATFORMS{" "}
+                  </p>
                 </div>
-
-                
               </div>
             </div>
+          </div>
         </div>
       </div>
     </>
