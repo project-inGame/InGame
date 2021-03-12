e87bf61dc8edf03a504374b59c5c44cd21a859e6
Joe Foster <jfoster29492@gmail.com>
2021-03-12 15:56:25 +0000

    hotfix

--- frontend/src/components/about/AboutPage.js
+++ frontend/src/components/about/AboutPage.js
@@ -6,31 +6,11 @@ const AboutPage = () => {
   return(
   <div className="aboutPageArea">
     <h1 id="aboutTitle">About INGAME</h1>
-    <img id="logo" src="../images/gamelogo.png" alt="logotop" />
+    <img id="logo" src="../images/gamelogo.png" a href="https://github.com/project-inGame/InGame" alt="logotop" />
     <p>
+      Created by <a href="https://github.com/Riley142">Riley</a><a href="https://github.com/mafromist">Muge</a><a href="https://github.com/JoeFoster-cn">Joe</a>and<a href="https://github.com/phiddle">Phil</a>
       {" "}
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque non
-      commodo magna, nec tincidunt odio. Aenean ut turpis eget urna commodo
-      feugiat eget eu quam. Phasellus eu tincidunt lacus. Quisque vitae neque
-      eget felis porttitor hendrerit. Nam ut suscipit ipsum. Fusce et laoreet
-      mi. Duis non lectus et ex egestas scelerisque quis id libero. Etiam sed
-      varius felis, non sollicitudin nisl. Integer semper suscipit vestibulum.
-      Pellentesque luctus lacinia turpis eget tincidunt. In porttitor erat quam,
-      vitae rutrum dui fermentum non. Integer commodo leo porttitor eros mollis
-      facilisis. Pellentesque suscipit metus a arcu tempus placerat. Aenean
-      turpis mauris, condimentum in lacus vel, aliquam pellentesque velit.
-      Nullam sem lacus, molestie sit amet nulla eu, pellentesque elementum
-      risus. Quisque tristique risus nec augue commodo, ac ultrices leo
-      tincidunt. Nunc eu est in libero pretium mattis. Morbi ultrices eros
-      libero, vitae rutrum orci feugiat eu. Praesent varius lobortis
-      pellentesque. Phasellus tellus diam, tincidunt ac odio et, semper lobortis
-      orci. Integer ullamcorper tempor feugiat. Proin efficitur, leo vitae
-      pellentesque dignissim, odio erat feugiat lorem, eu finibus tortor nunc
-      rutrum felis. Ut ut viverra velit. Ut venenatis finibus nisi, id viverra
-      velit porta nec. Integer non turpis ultrices, blandit velit sit amet,
-      pharetra odio. In aliquam metus vitae ornare vehicula. Mauris ultrices
-      enim sed risus semper ultrices. Fusce quam nunc, congue elementum nisi ut,
-      rhoncus convallis magna.{" "}
+      {" "}
     </p>
   </div>
   )
