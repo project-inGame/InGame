a5d35a0d1036b436a5455d0fbc25180d3afb550e
Muge <alev.muge@gmail.com>
2021-03-12 15:25:12 +0000

    #67 About Page and Appjs Footer global added

---
+++ frontend/src/components/about/AboutPage.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./aboutPage.css";
+
+const AboutPage = () => {
+  <div className="aboutPageArea">
+    <h1 id="aboutTitle">About INGAME</h1>
+    <img id="logo" src="../images/gamelogo.png" alt="logotop" />
+    <p>
+      {" "}
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque non
+      commodo magna, nec tincidunt odio. Aenean ut turpis eget urna commodo
+      feugiat eget eu quam. Phasellus eu tincidunt lacus. Quisque vitae neque
+      eget felis porttitor hendrerit. Nam ut suscipit ipsum. Fusce et laoreet
+      mi. Duis non lectus et ex egestas scelerisque quis id libero. Etiam sed
+      varius felis, non sollicitudin nisl. Integer semper suscipit vestibulum.
+      Pellentesque luctus lacinia turpis eget tincidunt. In porttitor erat quam,
+      vitae rutrum dui fermentum non. Integer commodo leo porttitor eros mollis
+      facilisis. Pellentesque suscipit metus a arcu tempus placerat. Aenean
+      turpis mauris, condimentum in lacus vel, aliquam pellentesque velit.
+      Nullam sem lacus, molestie sit amet nulla eu, pellentesque elementum
+      risus. Quisque tristique risus nec augue commodo, ac ultrices leo
+      tincidunt. Nunc eu est in libero pretium mattis. Morbi ultrices eros
+      libero, vitae rutrum orci feugiat eu. Praesent varius lobortis
+      pellentesque. Phasellus tellus diam, tincidunt ac odio et, semper lobortis
+      orci. Integer ullamcorper tempor feugiat. Proin efficitur, leo vitae
+      pellentesque dignissim, odio erat feugiat lorem, eu finibus tortor nunc
+      rutrum felis. Ut ut viverra velit. Ut venenatis finibus nisi, id viverra
+      velit porta nec. Integer non turpis ultrices, blandit velit sit amet,
+      pharetra odio. In aliquam metus vitae ornare vehicula. Mauris ultrices
+      enim sed risus semper ultrices. Fusce quam nunc, congue elementum nisi ut,
+      rhoncus convallis magna.{" "}
+    </p>
+  </div>;
+};
+
+export default AboutPage;
